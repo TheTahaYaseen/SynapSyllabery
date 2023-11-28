ccd4145182b560d426da379c957ceaad380c559d
The Taha Yaseen <thetahayaseen@gmail.com>
2023-11-28 17:15:27 +0500

    Just Some Standard Stuff Till Auht

--- syllabary/urls.py
+++ syllabary/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home_view, name="home"),
+    path('', views.home_view, name="home"),
     
     path('register', views.register_view, name="register"),
     path('login', views.login_view, name="login"),
